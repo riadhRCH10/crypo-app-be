67c872bf248df6b68c9dcf851f845fd2f7df5a27
riadhRCH <riadh@seeekmake.tech>
2023-11-19 22:40:17 +0100

    using process provided port

--- src/main.ts
+++ src/main.ts
@@ -5,22 +5,25 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = 3000
-  const globalPrefix = 'api'
+  const globalPrefix = 'api';
 
   const config = new DocumentBuilder()
-  .addBearerAuth()
-  .setTitle('Util')
-  .setDescription('The util API description')
-  .setVersion('1.0')
-  .build();
+    .addBearerAuth()
+    .setTitle('crypto app backend')
+    .setDescription('The crypto API description')
+    .setVersion('1.0')
+    .build();
 
   const document = SwaggerModule.createDocument(app, config);
-	SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000; // Use environment port or default to 3000
+  await app.listen(port);
+
   Logger.log(
-		`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
-	);
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+  );
 }
+
 bootstrap();
+
